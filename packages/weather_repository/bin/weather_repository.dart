a66b9cbd9c49562864c8e0ccd453081846c635b4
Samfan <samfan@live.com>
2024-02-12 19:05:18 +0530

    package init

---
+++ packages/weather_repository/bin/weather_repository.dart
@@ -0,0 +1,5 @@
+import 'package:weather_repository/weather_repository.dart' as weather_repository;
+
+void main(List<String> arguments) {
+  print('Hello world: ${weather_repository.calculate()}!');
+}
---
+++ packages/weather_repository/test/weather_repository_test.dart
@@ -0,0 +1,8 @@
+import 'package:weather_repository/weather_repository.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
