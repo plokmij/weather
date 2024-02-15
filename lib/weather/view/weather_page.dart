459dc079346b6e4f771313d26abbd194fc3b9343
Samfan <samfan@live.com>
2024-02-15 12:24:08 +0530

    initial version

--- lib/weather/view/weather_page.dart
+++ lib/weather/view/weather_page.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:weather_repository/weather_repository.dart';
@@ -43,21 +44,73 @@ class WeatherView extends StatelessWidget {
             case WeatherStatus.loading:
               return const WeatherLoading();
             case WeatherStatus.success:
-              return Column(
-                children: [
-                  Text(
-                    '${state.weather.temperature.toStringAsFixed(0)}°',
-                    style: const TextStyle(
-                      fontSize: 40,
-                      fontWeight: FontWeight.w500,
+              return Padding(
+                padding:
+                    MediaQuery.of(context).orientation == Orientation.landscape
+                        ? EdgeInsets.symmetric(
+                            horizontal: MediaQuery.of(context).size.width * 0.2)
+                        : const EdgeInsets.all(0),
+                child: Container(
+                  width: double.infinity,
+                  color: const Color(0xffBEC9CD),
+                  child: Padding(
+                    padding: MediaQuery.of(context).orientation ==
+                            Orientation.landscape
+                        ? const EdgeInsets.all(8.0)
+                        : const EdgeInsets.all(16),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Text(
+                              '${state.weather.temperature.toStringAsFixed(0)}°',
+                              style: const TextStyle(
+                                fontSize: 60,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                            CachedNetworkImage(
+                                imageUrl:
+                                    'https://openweathermap.org/img/wn/${state.weather.weatherDescription.icon}.png'),
+                            const Spacer(),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: [
+                                Text(
+                                  state.weather.weatherDescription.main,
+                                  style: const TextStyle(
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                                Text(
+                                  'Humidity: ${state.weather.humidity.toStringAsFixed(0)}%',
+                                  style: const TextStyle(
+                                    fontWeight: FontWeight.w300,
+                                  ),
+                                ),
+                                Text(
+                                  'Wind: ${state.weather.windSpeed.toStringAsFixed(0)}km/h',
+                                  style: const TextStyle(
+                                    fontWeight: FontWeight.w300,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                        Text(
+                          'High: ${state.weather.minTemperature.toStringAsFixed(0)}° '
+                          'Low: ${state.weather.maxTemperature.toStringAsFixed(0)}°',
+                          style: const TextStyle(
+                            fontWeight: FontWeight.w300,
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                  Text(state.weather.weatherDescription.main),
-                  Text('${state.weather.minTemperature.toStringAsFixed(0)}°'),
-                  Text('${state.weather.maxTemperature.toStringAsFixed(0)}°'),
-                  Text('${state.weather.humidity.toStringAsFixed(0)}%'),
-                  Text('${state.weather.windSpeed.toStringAsFixed(0)}km/h'),
-                ],
+                ),
               );
             case WeatherStatus.failure:
               return WeatherFailure(
